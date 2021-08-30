edc40857ee8f644747d25882c22b93ed59934c2b
bemolxd <bartosz.bem@bemideas.pl>
2021-08-30 22:24:44 +0200

    don't return password in response

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,8 +8,8 @@ import {
   Put,
   Query,
 } from '@nestjs/common';
-import { User } from 'src/db/entities/user.entity';
 import { IUserPayload } from './types/IUserPayload';
+import { IUserResponse } from './types/IUserResponse';
 import { UserService } from './user.service';
 
 @Controller('users')
@@ -17,31 +17,32 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
-  async getAll(@Query('name') name: string): Promise<IUserPayload[]> {
+  async getAll(@Query('name') name: string): Promise<IUserResponse[]> {
     if (name) return this.userService.getUsersByName(name);
     return this.userService.getAll();
   }
 
   @Post()
-  postCreateUser(@Body() payload: IUserPayload): Promise<User> {
+  postCreateUser(@Body() payload: IUserPayload): Promise<IUserResponse> {
     return this.userService.createUser(payload);
   }
 
   @Get(':id')
-  async getUserById(@Param('id') id: number): Promise<User> {
+  async getUserById(@Param('id') id: number): Promise<IUserResponse> {
     return this.userService.getOneById(id);
   }
 
   @Put(':id')
   async putUpdateUser(
     @Param('id') id: number,
-    @Body() payload: IUserPayload,
-  ): Promise<User> {
-    return this.userService.updateUser(id, payload);
+    @Body() username: string,
+    @Body() email: string,
+  ): Promise<IUserResponse> {
+    return this.userService.updateUser(id, username, email);
   }
 
   @Delete(':id')
-  async deleteUser(@Param('id') id: number): Promise<User> {
+  async deleteUser(@Param('id') id: number): Promise<Object> {
     return this.userService.deleteUser(id);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,12 +3,13 @@ import {
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
-import argon2 from 'argon2';
+import * as argon2 from 'argon2';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/db/entities/user.entity';
 import { validateEmail } from 'src/validation';
 import { Repository } from 'typeorm';
 import { IUserPayload } from './types/IUserPayload';
+import { IUserResponse } from './types/IUserResponse';
 
 @Injectable()
 export class UserService {
@@ -16,30 +17,53 @@ export class UserService {
     @InjectRepository(User) private usersRepository: Repository<User>,
   ) {}
 
-  getAll(): Promise<User[]> {
-    return this.usersRepository
+  async getAll(): Promise<IUserResponse[]> {
+    const users = await this.usersRepository
       .createQueryBuilder()
       .orderBy('id', 'DESC')
       .getMany();
+
+    return users.map((user) => {
+      const { password, ...rest } = user;
+      return rest;
+    });
   }
 
-  async getOneById(id: number): Promise<User> {
+  async getOneById(id: number): Promise<IUserResponse> {
     try {
-      const user = await this.usersRepository.findOneOrFail(id);
-      return user;
+      return await this.usersRepository.findOneOrFail(id).then((user) => {
+        const { password, ...rest } = user;
+        return rest;
+      });
     } catch {
       throw new NotFoundException(`User with id ${id} does not exist!`);
     }
   }
 
-  async getUsersByName(name: string): Promise<User[]> {
+  async getUsersByName(name: string): Promise<IUserResponse[]> {
     const users = await this.usersRepository.find();
-    return users.filter((user) =>
-      user.username.toLowerCase().includes(name.toLowerCase()),
-    );
+    return users
+      .filter((user) =>
+        user.username.toLowerCase().includes(name.toLowerCase()),
+      )
+      .map((user) => {
+        const { password, ...rest } = user;
+        return rest;
+      });
   }
 
-  async createUser(payload: IUserPayload): Promise<User> {
+  // only for login
+  async getOneByEmail(email: string): Promise<User> {
+    try {
+      const user = await this.usersRepository.findOneOrFail({ email });
+      return user;
+    } catch {
+      throw new NotFoundException(`User with email ${email} does not exist!`);
+    }
+  }
+
+  // only for register
+  async createUser(payload: IUserPayload): Promise<IUserResponse> {
     if (!validateEmail(payload.email)) {
       throw new BadRequestException('Email is not valid');
     }
@@ -55,28 +79,41 @@ export class UserService {
     const user = this.usersRepository.create(dto);
 
     try {
-      return this.usersRepository.save(user);
+      this.usersRepository.save(user);
+      const { password, ...rest } = user;
+      return rest;
     } catch (error) {
       console.error(error);
     }
   }
 
-  async updateUser(id: number, payload: IUserPayload): Promise<User> {
-    if (!validateEmail(payload.email)) {
-      throw new BadRequestException('Email is not valid');
-    }
+  async updateUser(
+    id: number,
+    username: string,
+    email: string,
+  ): Promise<IUserResponse> {
     const user = await this.getOneById(id);
 
-    user.username = payload.username;
-    user.email = payload.email;
-    user.password = payload.password;
+    if (!validateEmail(email)) {
+      throw new BadRequestException('Email is not valid');
+    }
+
+    user.username = username;
+    user.email = email;
 
     return this.usersRepository.save(user);
   }
 
-  async deleteUser(id: number): Promise<User> {
-    const user = await this.getOneById(id);
-
-    return this.usersRepository.remove(user);
+  async deleteUser(id: number): Promise<Object> {
+    try {
+      const user = await this.usersRepository.findOneOrFail(id);
+      this.usersRepository.remove(user);
+      return {
+        statusCode: 200,
+        message: 'User deleted',
+      };
+    } catch {
+      throw new NotFoundException(`User with id ${id} does not exist!`);
+    }
   }
 }

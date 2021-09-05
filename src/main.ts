06f1f3d0626a921908672100d137f7e2f876b365
bemolxd <bartosz.bem@bemideas.pl>
2021-09-05 12:40:34 +0200

    update cors policy

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import * as session from 'express-session';
 import * as passport from 'passport';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: { origin: 'http://localhost:3000', credentials: true },
+  });
 
   app.use(
     session({

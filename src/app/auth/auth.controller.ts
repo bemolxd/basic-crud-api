60d2bc6f99526534da9da26dfa5cf8054d4c84e0
bemolxd <bartosz.bem@bemideas.pl>
2021-09-06 21:27:31 +0200

    add auth/logout route

--- src/app/auth/auth.controller.ts
+++ src/app/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Controller, Post, UseGuards, Get } from '@nestjs/common';
 
-import { RequestObject } from 'src/app/types';
+import { RequestObject, RequestUser } from 'src/app/types';
 
 import { AuthenticatedGuard } from './guards/authenticated.guard';
 import { LocalAuthGuard } from './guards/local-auth.guard';
@@ -11,13 +11,19 @@ export class AuthController {
 
   @UseGuards(AuthenticatedGuard)
   @Get('me')
-  me(@Request() req: RequestObject) {
+  me(@Request() req: RequestObject): RequestUser {
     return req.user;
   }
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
-  login(@Request() req: RequestObject) {
+  login(@Request() req: RequestObject): RequestUser {
     return req.user;
+  }
+
+  @Get('logout')
+  logout(@Request() req: RequestObject) {
+    req.logOut();
+    return { statusCode: 200, message: 'Logged out' };
   }
 }
